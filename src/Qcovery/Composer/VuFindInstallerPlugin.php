05e056ca22f90d1e1bc0501600101c172c37d07c
Johannes Schultze <schultze@effective-webwork.de>
2022-02-10 13:45:26 +0100

    Update.

--- src/Qcovery/Composer/VuFindInstallerPlugin.php
+++ src/Qcovery/Composer/VuFindInstallerPlugin.php
@@ -6,11 +6,19 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-abstract class VuFindInstallerPlugin implements PluginInterface
+class VuFindInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new VuFindInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
     }
 }
