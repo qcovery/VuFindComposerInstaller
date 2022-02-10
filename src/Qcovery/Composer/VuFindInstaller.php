270cfea204d03eeed72a3948ca6c1b18ea120e91
Johannes Schultze <schultze@effective-webwork.de>
2022-02-10 13:09:54 +0100

    Update.

---
+++ src/Qcovery/Composer/VuFindInstaller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Qcovery\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class VuFindInstaller extends LibraryInstaller
+{
+    public function getInstallPath(PackageInterface $package)
+    {
+        if (!isset($package->getExtra()->moduleName) || $package->getExtra()->moduleName == '') {
+            throw new \InvalidArgumentException('Extra field moduleName is not set');
+        }
+        return 'module/'.$package->getExtra()->moduleName;
+    }
+}
---
+++ src/Qcovery/Composer/VuFindInstallerPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Qcovery\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class VuFindInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new VuFindInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
